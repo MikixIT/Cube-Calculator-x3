c6d6bb987ee8fbf535caf76b59d891f7e619e3ad
MikixIT <michael.46@live.it>
2022-08-22 16:05:58 +0200

    new project

---
+++ script.js
@@ -0,0 +1,20 @@
+let button = document.querySelector('button');
+let numberOne = document.querySelector('input');
+let result;
+let printResult = document.getElementById('print');
+
+button.addEventListener('click', ()=>{buttonclicked()});
+
+
+function cube(number){
+    result = number * number * number;
+    return result;
+} 
+
+function buttonclicked(){
+    if (isNaN(numberOne.value)) {
+        alert('Not a Number!');
+    } else {
+        printResult.innerHTML = cube(numberOne.value);
+    }
+}
